514b8ec905741624ad205d2a1051de801e38f599
Jack Chouchani <jack.chouchani@epita.fr>
2022-01-24 14:51:11 +0100

    big changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,20 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 const modalViews = document.querySelectorAll('.services__modal'),
-        modalBtns = document.querySelectorAll('.services__button'),
-        modalClose = document.querySelectorAll('.services__modal-close')
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
 
-let modal = function(modalClick){
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add('active-modal')
 }
 
-modalBtns.forEach((mb, i) =>{
-    mb.addEventListener('click', () =>{
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
         modal(i)
     })
 })
-modalClose.forEach((mc) =>{
-    mc.addEventListener('click', () =>{
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
         modalViews.forEach((mv) => {
             mv.classList.remove('active-modal')
         })
@@ -30,18 +30,18 @@ modalClose.forEach((mc) =>{
 })
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 let mixerPortfolio = mixitup('.work__container', {
-        selectors: {
-            target: '.work__card'
+    selectors: {
+        target: '.work__card'
     },
     animation: {
         duration: 300
     }
 });
-/* Link active work */ 
+/* Link active work */
 const linkWork = document.querySelectorAll('.work__item')
 
 
-function activeWork(){
+function activeWork() {
     linkWork.forEach(l => l.classList.remove('active-work'))
     this.classList.add('active-work')
 }
@@ -49,30 +49,76 @@ function activeWork(){
 linkWork.forEach(l => l.addEventListener('click', activeWork))
 /*=============== SWIPER TESTIMONIAL ===============*/
 var swiperTestimonial = new Swiper(".testimonial__container", {
-        spaceBetween: 24,
-        loop: true,
-        grabCursor: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
         },
-        breakpoints: {
-            576: {
-                slidesPerView: 2,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 48,
-            },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        },
 
-        }
-    })
+    }
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
 
-/*=============== LIGHT DARK THEME ===============*/ 
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
 
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' +
+                sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' +
+                sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+    window.addEventListener('scroll', scrollActive)
 
+    /*=============== LIGHT DARK THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the light / icon theme
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+    /*=============== SCROLL REVEAL ANIMATION ===============*/
