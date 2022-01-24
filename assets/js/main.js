a705629e3091bf195700f32be3898cb7e765017e
Jack Chouchani <jack.chouchani@epita.fr>
2022-01-24 17:36:18 +0100

    small typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,7 @@ function scrollActive() {
             sectionId = current.getAttribute('id')
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav_menu a[href*=' +
+            document.querySelector('.nav__menu a[href*=' +
                 sectionId + ']').classList.add('active-link')
         } else {
             document.querySelector('.nav__menu a[href*=' +
